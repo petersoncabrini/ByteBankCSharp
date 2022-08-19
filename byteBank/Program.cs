99f9d16bda54a53c1690357f9e684e49d58125c1
Peterson <petersoncabrini@hotmail.com>
2022-08-19 19:59:07 -0300

    Adicionar arquivos de projeto.

---
+++ byteBank/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Bem vindo(a) ao seu banco, ByteBank!");
+
+string titular = "Usuario";
+string conta = "10123-X";
+int numeroAgencia = 23;
+string nomeAgencia = "Agencia Central";
+double saldo = 93.50;
+
+Console.WriteLine("Titular: " + titular);
+Console.WriteLine("Conta: " + conta);
+Console.WriteLine("Numero da Agencia: " + numeroAgencia);
+Console.WriteLine("Nome da Agencia: " + nomeAgencia);
+Console.WriteLine("Saldo: " + saldo);
+
+Console.ReadKey();
