9348f57024b612b6d4c413b5e41c38a68014fc1a
Peterson <petersoncabrini@hotmail.com>
2022-08-19 20:11:37 -0300

    Criando a primeira classe

---
+++ byteBank/ContaCorrente.cs
@@ -0,0 +1,11 @@
+﻿namespace byteBank
+{   
+    public class ContaCorrente
+    {
+        public string titular;
+        public string conta;
+        public int numeroAgencia;
+        public string nomeAgencia;
+        public double saldo;
+    }
+}
