3bf79449a32f549f3550654829bb51128f2dc174
Peterson <petersoncabrini@hotmail.com>
2022-08-21 15:12:33 -0300

    Add metodos ExibirDadosDaConta, Depositar e Transferir à classe ContaCorrente

--- byteBank/ContaCorrente.cs
+++ byteBank/ContaCorrente.cs
@@ -1,28 +1,60 @@
 ﻿namespace byteBank
-{   
+{
     //A classe e um modelo para criar um objeto
     public class ContaCorrente
     {
         //Campos sao variaveis definidas em uma classe
-        public string titular; 
-        public string conta;   
-        public int numeroAgencia; 
+        public string titular;
+        public string conta;
+        public int numeroAgencia;
         public string nomeAgencia;
-        public double saldo;  
+        public double saldo;
 
-        public bool Sacar (double valor)
+        public void ExibirDadosDaConta()
+        {
+            Console.WriteLine("Titular :" + titular);
+            Console.WriteLine("Conta :" + conta);
+            Console.WriteLine("Número Agência :" + numeroAgencia);
+            Console.WriteLine("Nome Agência :" + nomeAgencia);
+            Console.WriteLine("Saldo: " + saldo);
+        }
+
+        public bool Sacar(double valor)
         {
             if (saldo < valor)
             {
                 return false;
             }
-            if (valor < 0)  
+            if (valor < 0)
             {
                 return false;
             }
             else
             {
                 saldo = saldo - valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo = saldo + valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
                 return true;
             }
         }
