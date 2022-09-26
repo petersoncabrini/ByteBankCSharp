128fffe24561d1d17a8522ca1ee69fdad8ba21ca
Peterson <petersoncabrini@hotmail.com>
2022-09-26 19:47:42 -0300

    Adicionado projeto byteBank_Admin

---
+++ byteBank_Admin/Program.cs
@@ -0,0 +1,10 @@
+﻿using byteBank_Admin.Funcionarios;
+
+Console.WriteLine("Boas vidas, ao ByteBank Administracao");
+
+Funcionario joao = new Funcionario();
+joao.Nome = "Joao";
+joao.Cpf = "123456789";
+joao.Salario = 2000;
+
+Console.WriteLine("Bonificacao: " + joao.getBonificacao());
