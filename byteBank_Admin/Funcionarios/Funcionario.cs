3de754e609f821b531f6c8d11cc7f2428a8e9e60
Peterson <petersoncabrini@hotmail.com>
2022-09-27 20:38:46 -0300

    Implementando tipos de funcionario e bonificacao de acordo com o tipo.

--- byteBank_Admin/Funcionarios/Funcionario.cs
+++ byteBank_Admin/Funcionarios/Funcionario.cs
@@ -8,14 +8,33 @@ namespace byteBank_Admin.Funcionarios
 {
     public class Funcionario
     {
+        // 0 - funcionario
+        // 1 - diretor
+        // 2 - designer
+        // N - ...
+        private int _tipo;
+
+        //Construtor
+        public Funcionario (int _tipo)
+        {
+            this._tipo = _tipo;
+        }
+
         public string Nome { get; set; }
         public string Cpf { get; set; }
         public double Salario { get; set; }
 
+        // Metodo para obter a bonificacao conforme o tipo do funcionario
         public double getBonificacao()
         {
-            return Salario * 0.1;
+            if (_tipo == 1)
+            {
+                return Salario;
+            }
+            else
+            {
+                return Salario * 0.1;
+            }
         }
-        
     }
 }
--- byteBank_Admin/Program.cs
+++ byteBank_Admin/Program.cs
@@ -2,9 +2,12 @@
 
 Console.WriteLine("Boas vidas, ao ByteBank Administracao");
 
-Funcionario joao = new Funcionario();
+Funcionario joao = new Funcionario(1);
 joao.Nome = "Joao";
 joao.Cpf = "123456789";
 joao.Salario = 2000;
 
+Console.WriteLine("Nome: " + joao.Nome);
+Console.WriteLine("CPF: " + joao.Cpf);
+Console.WriteLine("Salario: " + joao.Salario);
 Console.WriteLine("Bonificacao: " + joao.getBonificacao());
