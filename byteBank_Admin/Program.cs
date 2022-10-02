78590377de3296cf1485a779d7035a64dadf09e3
Peterson <petersoncabrini@hotmail.com>
2022-10-02 15:25:05 -0300

    add class GerenciadorDeBonificacao

--- byteBank_Admin/Program.cs
+++ byteBank_Admin/Program.cs
@@ -1,18 +1,26 @@
 ﻿using byteBank_Admin.Funcionarios;
+using byteBank_Admin.Utilitario;
 
 Console.WriteLine("Boas vidas, ao ByteBank Administracao");
+
+GerenciadorDeBonificacao gerenciador = new GerenciadorDeBonificacao();
 
 Funcionario joao = new Funcionario();
 joao.Nome = "Joao";
 joao.Cpf = "123456789";
 joao.Salario = 2000;
 
-Console.WriteLine("Nome: " + joao.Nome);
-Console.WriteLine("CPF: " + joao.Cpf);
-Console.WriteLine("Salario: " + joao.Salario);
-Console.WriteLine("Bonificacao: " + joao.getBonificacao());
+//Console.WriteLine("Nome: " + joao.Nome);
+//Console.WriteLine("CPF: " + joao.Cpf);
+//Console.WriteLine("Salario: " + joao.Salario);
+//Console.WriteLine("Bonificacao: " + joao.getBonificacao());
 
 Diretor paula = new Diretor();
 paula.Nome = "Paula";
 paula.Cpf = "987654321";
 paula.Salario = 5000;
+paula.getBonificacao();
+
+gerenciador.Registrar(joao);
+gerenciador.Registrar(paula);
+Console.WriteLine("Total de Bonificacao:" + gerenciador.getBonificacao());
---
+++ byteBank_Admin/Utilitario/GerenciadorDeBonificacao.cs
@@ -0,0 +1,29 @@
+﻿using byteBank_Admin.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace byteBank_Admin.Utilitario
+{
+    public class GerenciadorDeBonificacao
+    {
+        private double totalBonificacao;
+        public void Registrar(Funcionario funcionario)
+        {
+            this.totalBonificacao += funcionario.getBonificacao();
+        }
+        public void Registrar(Diretor diretor)
+        {
+            this.totalBonificacao += diretor.getBonificacao();
+        }
+
+        public double getBonificacao()
+        {
+            return this.totalBonificacao;
+        }
+    }
+
+
+}
